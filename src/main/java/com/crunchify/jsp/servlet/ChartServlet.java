655341269b2f5e456430abad709041aa3a5a59cf
grey <grey@GREY4210.academia.loc>
2017-06-01 15:56:40 -0500

    bar

--- src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
+++ src/main/java/com/crunchify/jsp/servlet/ChartServlet.java
@@ -15,10 +15,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.CategoryAxis;
+import org.jfree.chart.axis.CategoryLabelPositions;
+import org.jfree.chart.plot.CategoryPlot;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.renderer.category.BarRenderer;
+import org.jfree.chart.renderer.category.CategoryItemRenderer;
+import org.jfree.data.category.CategoryDataset;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
 import org.jfree.data.general.DefaultPieDataset;
-
 public class ChartServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
@@ -51,6 +61,43 @@ public class ChartServlet extends HttpServlet {
 		chart.setBorderVisible(true);
 
 		return chart;
+	}
+        
+        public JFreeChart getChar2t() {
+		
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        dataset.addValue(25.0, "Series 1", "Category 1");   
+        dataset.addValue(34.0, "Series 1", "Category 2");   
+        dataset.addValue(19.0, "Series 2", "Category 1");   
+        dataset.addValue(29.0, "Series 2", "Category 2");   
+        dataset.addValue(41.0, "Series 3", "Category 1");   
+        dataset.addValue(33.0, "Series 3", "Category 2");   
+
+		
+        JFreeChart chart = ChartFactory.createBarChart3D(
+            "3D Bar Chart Demo",      // chart title
+            "Category",               // domain axis label
+            "Value",                  // range axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL, // orientation
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+        CategoryPlot plot = chart.getCategoryPlot();
+        CategoryAxis axis = plot.getDomainAxis();
+        axis.setCategoryLabelPositions(
+            CategoryLabelPositions.createUpRotationLabelPositions(Math.PI / 8.0)
+        );
+        
+        CategoryItemRenderer renderer = plot.getRenderer();
+        renderer.setItemLabelsVisible(true);
+        BarRenderer r = (BarRenderer) renderer;
+        r.setMaximumBarWidth(0.05);
+        return chart;
+
+		
 	}
 
 }
